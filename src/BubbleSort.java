81129e89dfa77899e5ca19b6fdf822ad773a5679
ARUN-AK5011 <thedeveloper.arun@gmail.com>
2026-01-18 21:59:01 +0530

    Updated bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 7, 4, 2};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    static int[] sort(int[] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(!swapped){break;}
+        }
+        return arr;
+    }
+}
